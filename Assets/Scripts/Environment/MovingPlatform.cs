a4e8064a10826bc6ba454d74098e52e5a224aa38
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2022-02-05 13:30:35 -0800

    Moving Platform Work

--- Assets/Scripts/Environment/MovingPlatform.cs
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -7,8 +7,13 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] Vector2 start;
     [SerializeField] Vector2 end;
     [SerializeField] GameObject trackRef;
+    Vector2 endDir;
+    Vector2 workspace;
     GameObject track;
     LineRenderer lr;
+    [SerializeField] float movePercentage = 0.02f;
+    [SerializeField] float maxMoveSpeed = 2;
+    bool movingToEnd = true;
 
     // Start is called before the first frame update
     void Start()
@@ -29,12 +34,35 @@ public class MovingPlatform : MonoBehaviour
         lr.SetPosition(0, new Vector2(start.x, start.y));
         lr.SetPosition(1, new Vector2(end.x, end.y));
 
+        endDir = (end - start).normalized;
         transform.position = start;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
+        // TODO: Change velocity instead of changing position
+
+        if (movingToEnd)
+        {
+            workspace.Set(Vector2.Lerp(transform.position, end, movePercentage).x, Vector2.Lerp(transform.position, end, movePercentage).y);
+            Vector2.ClampMagnitude(workspace, maxMoveSpeed);
+            transform.position = workspace;
+        }
+        else
+        {
+            workspace.Set(Vector2.Lerp(transform.position, start, movePercentage).x, Vector2.Lerp(transform.position, start, movePercentage).y);
+            Vector2.ClampMagnitude(workspace, maxMoveSpeed);
+            transform.position = workspace;
+        }
+        if(movingToEnd && Vector2.Distance(transform.position, end) < 0.03)
+        {
+            movingToEnd = !movingToEnd;
+        }
+        if (!movingToEnd && Vector2.Distance(transform.position, start) < 0.03)
+        {
+            movingToEnd = !movingToEnd;
+        }
     }
 }
